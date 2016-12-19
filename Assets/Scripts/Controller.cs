5a4e99d268180c3dd229fc6e33b99186441f8fb5
朱思能 <158590301@qq.com>
2016-12-19 19:04:06 +0800

    第一版

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+	public int numberOfPlayers = 9;
+	public Button seat;
+	public Canvas canvas;
+
+	void Start () {
+		for (int i = 0; i < numberOfPlayers; i++) {
+			Button copySeat = Instantiate (seat);
+			copySeat.transform.SetParent (canvas.transform, false);
+			copySeat.transform.position = new Vector2 (0, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
+		float x = sprite.bounds.size.x;
+		float y = sprite.bounds.size.y;
+		float scale = (y / x) * ((float)Screen.width / (float)Screen.height);
+
+		Vector2 localScale = sprite.transform.localScale;
+		localScale.x = scale;
+		sprite.transform.localScale = localScale;
+	}
+}
