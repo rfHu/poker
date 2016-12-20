853e51207cec27633152c6dbbe06d7035f160036
朱思能 <158590301@qq.com>
2016-12-20 15:49:13 +0800

    VSCode开发

---
+++ Assets/Scripts/StatusBar.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour {
+	void Start () {
+		DateTime time = System.DateTime.Now;	
+		String timeStr = time.ToString("hh:mm");	
+		
+		GetComponent<Text>().text = timeStr;
+	}
+}
