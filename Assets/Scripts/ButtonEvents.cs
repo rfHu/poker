64af78b4bba24345c63843a39923a487e42ba49e
朱思能 <158590301@qq.com>
2016-12-21 14:35:45 +0800

    加入事件

---
+++ Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ButtonEvents : MonoBehaviour {
+	public void selectMenu() {
+
+	}
+
+	bool menuShow = false;
+
+	public GameObject menu;
+
+	public void toggleMenu() {
+		Animation anim = menu.GetComponent<Animation>();
+
+		if (menuShow) {
+			anim.Play("Dropup");			
+		} else {
+			anim.Play("Dropdown");
+		}
+
+		menuShow = !menuShow;		
+	}
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -104,4 +103,6 @@ public class Controller : MonoBehaviour {
 
 		return new Vector2[]{};
 	}
+
+	
 }
