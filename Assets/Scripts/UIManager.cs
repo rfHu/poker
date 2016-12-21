b772c6ca6cbc73883a0c86dac2075231141bcfe3
朱思能 <158590301@qq.com>
2016-12-21 16:11:41 +0800

    调试

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+	public void selectMenu() {
+
+	}
+
+	// public GameObject menu;
+
+	public void hideMenu() {
+		// Animation anim = menu.GetComponent<Animation>();
+		// anim.Play("Dropup");			
+	}
+
+	public void showMenu() {
+		// Animation anim = menu.GetComponent<Animation>();
+		// anim.Play("Dropdown");
+	}
+}
