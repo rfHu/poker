fb4fc05cc5a4c9815c7510d3b9c97fe197b13ab6
朱思能 <158590301@qq.com>
2016-12-22 18:04:22 +0800

    menu完成

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -18,4 +18,20 @@ public class UIManager : MonoBehaviour {
 		mask.GetComponent<RectTransform>().localScale = new Vector3(0, 1, 1);
 		menu.GetComponent<Animator>().SetBool("Show", false);
 	}
+
+	public void Standup() {
+		Debug.Log("My God");
+	}
+
+	public void Exit() {
+		Application.Quit();
+	}
+
+	public void CardTip() {
+		Debug.Log("Tip");
+	}
+
+	public void Supplement() {
+		Debug.Log("Supplement");
+	}
 }
