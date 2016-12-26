b42d78be24125ce7c8580f190d5be151ab7783de
朱思能 <158590301@qq.com>
2016-12-26 19:57:45 +0800

    生成边栏界面

---
+++ Assets/Scripts/ScoreCtrl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ScoreCtrl : MonoBehaviour {
+	public GameObject scoreEntry;
+	public GameObject viewport;
+	List<Dictionary<string, object>> playerScoreList = new List<Dictionary<string, object>>();
+
+	void Start()
+	{
+		foreach(int i in Enumerable.Range(0, 7)) {
+			playerScoreList.Add(
+			new Dictionary<string, object>(){
+				{"buy", 100},
+				{"gain", 1000},
+				{"name", "singno"}
+			}
+			);
+		}
+
+		foreach(Dictionary<string, object> player in playerScoreList) {
+			 GameObject  entry = Instantiate(scoreEntry);
+			 entry.transform.Find("Name").GetComponent<Text>().text = (string)player["name"];
+			 entry.transform.Find("Total").GetComponent<Text>().text = ((int)player["buy"]).ToString(); 
+			 entry.transform.Find("Score").GetComponent<Text>().text = ((int)player["gain"]).ToString();
+
+			 entry.transform.SetParent(viewport.transform, false);
+		}
+	}
+}
