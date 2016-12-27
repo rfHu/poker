a7f52e1f243acc26a89991acbc5e5671164efda0
朱思能 <158590301@qq.com>
2016-12-27 21:02:39 +0800

    Player拆分

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -9,7 +9,6 @@ public class Controller : MonoBehaviour {
 
 	public GameObject gameInfo;
 	public GameObject gameInfoParent;
-	public GameObject playerPrefab;
 
 	public Dictionary<string, object> gameInfoDictionary;
 
@@ -31,7 +30,7 @@ public class Controller : MonoBehaviour {
 			int identifer = iter;
 			Vector2 vector = vectors[identifer];
 			button.onClick.AddListener(() => {
-				ShowPlayer(vector);	
+				// ShowPlayer(vector);	
 			});
 
 			iter++;
@@ -139,28 +138,4 @@ public class Controller : MonoBehaviour {
 		label.GetComponent<Text>().text = text;
 		label.transform.SetParent(gameInfoParent.transform, false);
 	}
-
-	void  ShowPlayer(Vector2 vector) {
-		Player player = new Player();	
-		player.avatar = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3081053742,1983158129&fm=116&gp=0.jpg";
-		player.name = "singno";
-		player.score = 50000;
-
-		GameObject playerObject = Instantiate(playerPrefab);
-		playerObject.transform.Find("Name").GetComponent<Text>().text = player.name ;
-		playerObject.transform.Find("Button").Find("Coins").GetComponent<Text>().text = player.score.ToString();
-		Transform avbg = playerObject.transform.Find("AvatarBg");
-		RawImage rawImage = avbg.Find("Avatar").GetComponent<RawImage>();
-		playerObject.transform.SetParent(canvas.transform,  false);
-		playerObject.GetComponent<RectTransform>().localPosition = vector;
-
-		// Ext.Circular(avbg.GetComponent<RawImage>().texture);
-		StartCoroutine(DownloadAvatar(rawImage, player.avatar));
-	}
-
-	IEnumerator<WWW> DownloadAvatar(RawImage img, string url) {
-		WWW www = new WWW(url);
-		yield return www;
-		img.texture = Ext.Circular(www.texture);
-	}		
 }
---
+++ Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerObject : MonoBehaviour {
+	public GameObject playerPrefab;
+	public Canvas canvas;
+	Player player;
+
+	PlayerObject(Player player, Vector2 vector) {
+		this.player = player;
+		ShowPlayer(vector);
+	}
+
+	GameObject ShowPlayer(Vector2 vector) {
+		Player player = new Player();	
+		player.avatar = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3081053742,1983158129&fm=116&gp=0.jpg";
+		player.name = "singno";
+		player.score = 50000;
+
+		GameObject playerObject = Instantiate(playerPrefab);
+		playerObject.transform.Find("Name").GetComponent<Text>().text = player.name ;
+		playerObject.transform.Find("Button").Find("Coins").GetComponent<Text>().text = player.score.ToString();
+		RawImage rawImage = playerObject.transform.Find("Circle").Find("Avatar").GetComponent<RawImage>();
+		playerObject.transform.SetParent(canvas.transform,  false);
+		playerObject.GetComponent<RectTransform>().localPosition = vector;
+
+		StartCoroutine(DownloadAvatar(rawImage, player.avatar));
+
+		return playerObject;
+	}
+
+	IEnumerator<WWW> DownloadAvatar(RawImage img, string url) {
+		WWW www = new WWW(url);
+		yield return www;
+		img.texture = Ext.Circular(www.texture);
+	}	
+	
+	void Update () {
+		
+	}
+}
