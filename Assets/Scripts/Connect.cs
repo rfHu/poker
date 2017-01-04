870b8bca21c73be4ce4d477fbe50ca1232720144
朱思能 <158590301@qq.com>
2017-01-04 17:21:06 +0800

    cin

---
+++ Assets/Scripts/Connect.cs
@@ -0,0 +1,13 @@
+﻿using BestHTTP.SocketIO;
+using System;
+
+public class Connect  {
+	private SocketManager manager;
+	private string url = "http://61.143.225.47:7001/"; 
+
+	Connect() {
+		// Sock
+		manager = new SocketManager(new Uri("http://61.143.225.47:7001/"), );
+		// manager = new SocketManager(new );
+	}
+}
