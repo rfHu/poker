529d91d841671721f460177dd8fd2b64f157bd49
朱思能 <158590301@qq.com>
2017-01-04 12:03:56 +0800

    卡片

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour {
+	public Sprite[] faces;
+	public Sprite cardBack;
+	public int cardIndex;
+
+	void Awake() {
+		GetComponent<SpriteRenderer>().sprite = cardBack;
+	}
+
+	public void Show() {	
+		GetComponent<Image>().sprite = faces[cardIndex];
+	}
+
+	public void Turnback() {
+		GetComponent<Image>().sprite = cardBack;
+	}
+}
