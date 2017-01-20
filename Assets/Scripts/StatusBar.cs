8f68810315c35bf47d8376160d97345b8fd76ac4
朱思能 <158590301@qq.com>
2017-01-20 15:53:13 +0800

    去掉电量获取代码

--- Assets/Scripts/StatusBar.cs
+++ Assets/Scripts/StatusBar.cs
@@ -33,54 +33,6 @@ public class StatusBar : MonoBehaviour {
 
 	public static float GetBatteryLevel()
     {
-        #if UNITY_IOS
-		return 100f;
-        // UIDevice device = UIDevice.CurrentDevice();
-        // device.batteryMonitoringEnabled = true; // need to enable this first
-        // Debug.Log("Battery state: " + device.batteryState);
-        // Debug.Log("Battery level: " + device.batteryLevel);
-        // return device.batteryLevel*100;
-        #elif UNITY_ANDROID
- 
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            try
-            {
-                using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-                {
-                    if (null != unityPlayer)
-                    {
-                        using (AndroidJavaObject currActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
-                        {
-                            if (null != currActivity)
-                            {
-                                using (AndroidJavaObject intentFilter = new AndroidJavaObject("android.content.IntentFilter", new object[]{ "android.intent.action.BATTERY_CHANGED" }))
-                                {
-                                    using (AndroidJavaObject batteryIntent = currActivity.Call<AndroidJavaObject>("registerReceiver", new object[]{null,intentFilter}))
-                                    {
-                                        int level = batteryIntent.Call<int>("getIntExtra", new object[]{"level",-1});
-                                        int scale = batteryIntent.Call<int>("getIntExtra", new object[]{"scale",-1});
- 
-                                        // Error checking that probably isn't needed but I added just in case.
-                                        if (level == -1 || scale == -1)
-                                        {
-                                            return 50f;
-                                        }
-                                        return ((float)level / (float)scale) * 100.0f;
-                                    }
-                               
-                                }
-                            }
-                        }
-                    }
-                }
-            } catch (System.Exception)
-            {
-             
-            }
-        }
-     
         return 100;
-        #endif
     }
 }
