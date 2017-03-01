5580e94f65a2d7d895ee0d3e98e51b39def168f7
朱思能 <158590301@qq.com>
2017-03-01 17:37:26 +0800

    删除没用到的文件

--- Assets/LeanLoader/Examples/SimpleLoadingImage.js
+++
@@ -1,8 +0,0 @@
-
-function Start () {
-	LeanLoader.load("http://dentedpixel.com/assets/Monkeyshines.png", LLOptions().setOnLoad(onImageLoaded).setUseFile(true));
-}
-
-private function onImageLoaded( tex:Texture2D ){
-	Debug.Log("Your image texture ready to use! :"+tex);
-}
