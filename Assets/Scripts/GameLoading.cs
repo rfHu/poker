09acca6a5d05e369f6ac2d7f7465dec8c7183ac2
朱思能 <158590301@qq.com>
2017-04-12 10:18:10 +0800

    测试

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -11,7 +11,7 @@ public class GameLoading : MonoBehaviour {
 
         external.SetSid("s%3ASAbXEJECbr6EIhP-VFSnAKfbgpHkWM-4.7ZJkZ23kXVZA1F7s%2FirnHteuWcBTma%2BIii5OhqmUKyQ");
 			external.SetProxy("http://localhost:8888");
-            external.SetRoomID("58ec3fedab6ca1757924d662");
+            external.SetRoomID("58ecbda28e5fb610794746c2");
 
 
 		#endif
