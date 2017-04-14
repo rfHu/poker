7db7937097ccfcbdcbebf2ecb404cceada974151
rfHu <2449663340@qq.com>
2017-04-14 17:27:40 +0800

    labber emoticon

--- Assets/Scripts/EmoticonEvemt.cs
+++ Assets/Scripts/EmoticonEvemt.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DarkTonic.MasterAudio;
+using DG.Tweening;
 
 public class EmoticonEvemt : MonoBehaviour {
 
     public GameObject tomatoPart2;
 
+    public GameObject laberPart2;
+    public RectTransform[] ice;
+
+    int times = 0;
+
+    float[] posX = { -120, 29, 132 };
+
     public void Destroy()
     {
         GameObject.Destroy(transform.parent.gameObject);
     }
+
 
 
     public void Destroythis() {
@@ -21,6 +30,16 @@ public class EmoticonEvemt : MonoBehaviour {
     public void TomatoPart2() 
     {
         tomatoPart2.SetActive(true);
+    }
+
+    public void LabberPart2() 
+    {
+        laberPart2.SetActive(true);
+        for (int i = 0; i < ice.Length; i++)
+		{
+            ice[i].DOLocalMoveX(posX[i], 0.3f).SetEase(Ease.Linear);
+            ice[i].DOLocalMoveY(-95, 0.3f).SetEase(Ease.OutBounce);
+	    }
     }
 
     public void LabberSound() 
