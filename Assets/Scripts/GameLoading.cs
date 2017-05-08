f95954ef4731cbd8f4a3850bcec8399ccb9f83bf
rfHu <2449663340@qq.com>
2017-05-08 14:29:09 +0800

    保险UI更改

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -8,9 +8,9 @@ public class GameLoading : MonoBehaviour {
 		var external = External.Instance;
 
 		#if UNITY_EDITOR  
-        external.SetSid("s%3A7d1azZBKnZ_IhBZKTGL1fzBdFTm-Cnfq.xUqmY3tvqD2DH7Fq2qZAV6j9eYVYHYd2kvIUPOniN3M");
+        external.SetSid("s%3Awo5ufTp8DAcxxnkzMaiM401PsdgQNBNv.5rdImjJHRoEQvf8yX6M%2BHV2TJxtW5rgFIZucx2NnvVQ");
 			external.SetProxy("http://localhost:8888");
-            external.SetRoomID("590c20c2b1628514ae36f638");
+            external.SetRoomID("590fcd0c2a6250572a5df475");
 
 		#endif
 	}
