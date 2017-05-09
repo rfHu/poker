565fb94f2fc4e30201d2b1501db2dd4590f2ae78
朱思能 <158590301@qq.com>
2017-05-09 18:43:48 +0800

    修改保险

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -8,9 +8,9 @@ public class GameLoading : MonoBehaviour {
 		var external = External.Instance;
 
 		#if UNITY_EDITOR  
-        external.SetSid("s%3Awo5ufTp8DAcxxnkzMaiM401PsdgQNBNv.5rdImjJHRoEQvf8yX6M%2BHV2TJxtW5rgFIZucx2NnvVQ");
+        external.SetSid("s%3A7d1azZBKnZ_IhBZKTGL1fzBdFTm-Cnfq.xUqmY3tvqD2DH7Fq2qZAV6j9eYVYHYd2kvIUPOniN3M");
 			external.SetProxy("http://localhost:8888");
-            external.SetRoomID("591182e556a4e61e8a71f4e6");
+            external.SetRoomID("59113d88c4eda3558fab29b5");
 
 		#endif
 	}
