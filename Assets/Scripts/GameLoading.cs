1cf89a5489583537333f59dec806ef8a6bd78b2b
rfHu <2449663340@qq.com>
2017-05-10 20:37:03 +0800

    去除2.1新效果

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -8,9 +8,9 @@ public class GameLoading : MonoBehaviour {
 		var external = External.Instance;
 
 		#if UNITY_EDITOR  
-        external.SetSid("s%3A7d1azZBKnZ_IhBZKTGL1fzBdFTm-Cnfq.xUqmY3tvqD2DH7Fq2qZAV6j9eYVYHYd2kvIUPOniN3M");
+        external.SetSid("s%3Awo5ufTp8DAcxxnkzMaiM401PsdgQNBNv.5rdImjJHRoEQvf8yX6M%2BHV2TJxtW5rgFIZucx2NnvVQ");
 			external.SetProxy("http://localhost:8888");
-            external.SetRoomID("5912c0d256a4e61e8a720722");
+            external.SetRoomID("5912e2e129bbaa73a278b7a9");
 
 		#endif
 	}
