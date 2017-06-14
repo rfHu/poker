9e124f4f852e429cbcf5d3e60fd32f102b8bd196
rfHu <2449663340@qq.com>
2017-06-14 15:12:13 +0800

    删除注释的代码

--- Assets/Scripts/Insurance.cs
+++ Assets/Scripts/Insurance.cs
@@ -107,7 +107,6 @@ public class Insurance : MonoBehaviour {
             playerMes.transform.SetParent(AllinPlayer.transform.parent,false);
             playerMes.SetActive(true);
             playerMes.GetComponent<AllInPlayer>().Init(player.Name, player.Cards.Value, player.Uid, outsNumber);
-            // PlayerList.sizeDelta += new Vector2(230, 0);
         }
     }
 
