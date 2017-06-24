c2067b0cf6f19bb8589761122ae5c22b463cf6d6
rfHu <2449663340@qq.com>
2017-06-24 11:04:58 +0800

    还原保险打开

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -9,10 +9,10 @@ public class GameLoading : MonoBehaviour {
 
 		#if UNITY_EDITOR  
         	// external.SetSid("s%3AdOlm2w5hGhqahgt--HMRhxyXjTCSmOLf.3IXxXmGw1R3E0Swr10JEcqYcGvn7AUZT4%2F0d99jOuT8");
-			external.SetSocket("https://socket.poker.top");
-        	external.SetSid("s%3A4gHJE4AJbNY5j6XT2OuoeO4-nqn6bl7t.5f5shYMZ2sFNrz0CKTpO5b2cUJujPknpA5OU7lThbJI");
+			external.SetSocket("https://socket.dev.poker.top");
+            external.SetSid("s%3AAN0aHRtcPLOAcsyLXRhv4ILXiVBkKYHt.DW9FrsSPSLXYPOxgUEYYJyYRdWGzgdKwLacaykHdbpM");
 			external.SetProxy("http://localhost:8888");
-            external.SetRoomID("594d0f3e532d9919716bb881");
+            external.SetRoomID("594dd276a697787ff4f72a92");
 		#endif
 	}
 	
