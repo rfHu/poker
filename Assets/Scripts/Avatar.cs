a08c9a823a09cc3063dd8bd0627794b07ec56ad9
朱思能 <158590301@qq.com>
2017-06-26 12:50:29 +0800

    对象池

--- Assets/Scripts/Avatar.cs
+++ Assets/Scripts/Avatar.cs
@@ -8,11 +8,66 @@ using System.Collections.Generic;
 
 // [RequireComponent(typeof(RawImage))]
 public class Avatar : MonoBehaviour {
+	public class TexturePool {
+		private Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
+		private Dictionary<String, int> counterHash = new Dictionary<string, int>();
 
-    public Material Opaque;
-    public Material Crystal;
+		private int max = 10;
+
+		private TexturePool() {}
+
+		static public TexturePool Shared = new TexturePool();
+
+		public bool Exists(string url) {
+			return textures.ContainsKey(url);
+		}
+
+		private void store(string url, Texture2D texture) {
+			textures[url] = texture;
+		}
+
+		private int insCounter(string url) {
+			var count = counterHash.Int(url) + 1;
+			counterHash[url] = count;
+			return count;
+		}
+
+		private int desCounter(string url) {
+			var count = counterHash.Int(url) - 1;
+			counterHash[url] = count;
+			return count;
+		}
+
+		public void Despawn(string url) {
+			var count = desCounter(url);
+
+			if (textures.Count > max && Exists(url) && count <= 0) {
+				var tex = textures[url];
+				textures.Remove(url);
+				counterHash.Remove(url);
+				GameObject.Destroy(tex);
+
+				_.Log("回收Texture");
+			}
+		} 
+
+		public void Spawn(string url, Action<Texture2D> cb) {
+			if (Exists(url)) {
+				insCounter(url);
+				cb(textures[url]);
+			} else {
+				_.LoadTexture(url, (texture) => {
+					insCounter(url);
+					store(url, texture);
+					cb(texture);
+				});
+			}
+		}
+	}
 
 	public string Uid;
+	public Material FullMat;
+	public Material AlphaMat;
 
 	public GameObject ClickObject;
 
@@ -34,9 +89,13 @@ public class Avatar : MonoBehaviour {
 		Regex rgx = new Regex(pattern);
 		url = rgx.Replace(url, "/w/124/h/124/format/png");
 
+		if (!string.IsNullOrEmpty(this.url)) {
+			TexturePool.Shared.Despawn(this.url);			
+		}
+
 		this.url = url;
 
-		TexturePool.Shared.FetchTexture(url, (texture) => {
+		TexturePool.Shared.Spawn(url, (texture) => {
 			if (this == null) {
 				return ;
 			}
@@ -56,13 +115,15 @@ public class Avatar : MonoBehaviour {
 
     public void SetAlpha(bool isInRoom) 
     {
+		var img = GetComponent<RawImage>();
+
         if (isInRoom)
         {
-            gameObject.GetComponent<RawImage>().material = (Material)Resources.Load("Materials/Mask");
+            img.material = FullMat;
         }
         else
         {
-            gameObject.GetComponent<RawImage>().material = (Material)Resources.Load("Materials/Mask.6");
+            img.material = AlphaMat;
         }
     }
 }
