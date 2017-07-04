3521db844234562bfeaf1834fa41cdb416f4bb9c
rfHu <2449663340@qq.com>
2017-07-04 20:01:02 +0800

    不知道改了什么但是插件可以用了

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -13,21 +13,20 @@ public class GameLoading : MonoBehaviour {
 			external.SetProxy("http://localhost:8888");
 			// external.SetSid("s%3ASuOxhyXR0wmfu1xIZZUmFFdAq8Cd5xsD.h6RldH4l7niXxoZG0QyYnlt36QayIW0rtY0Z8GmorV0");
             // external.SetRoomID("5950a958cfb1ed3a3860c871");
-
-			external.SetSid("s%3Ayzoz9QLBjEnL_Mg-ZbPDwEagrsmK7yFN.UqyioUkyg7j757Sk80TogBT4PPlB6rH0KtNsWo1qP4U");
-            external.SetRoomID("595b2f9638024a270844c9e6");
+            external.SetSid("s%3A-aQSxLwvnOxfxaFXAp3X_uVVclly1SuL.byIzHdOevZTTUaiLDNts%2FAm0dDOCj%2BecQlC89yAm6tU");
+            external.SetRoomID("5959ee649bf0960db7119b61");
 
 		#endif
 
 		// 开启SDK的日志打印，发布版本请务必关闭
-		// BuglyAgent.ConfigDebugMode (true);
+		//BuglyAgent.ConfigDebugMode (true);
 		// 注册日志回调，替换使用 'Application.RegisterLogCallback(Application.LogCallback)'注册日志回调的方式
 		// BuglyAgent.RegisterLogCallback (CallbackDelegate.Instance.OnApplicationLogCallbackHandler);
-		#if UNITY_IPHONE || UNITY_IOS
-			BuglyAgent.InitWithAppId("b3d868488f");
-		#elif UNITY_ANDROID
-			BuglyAgent.InitWithAppId ("a8103a785a");
-		#endif
+#if UNITY_IPHONE || UNITY_IOS
+            BuglyAgent.InitWithAppId ("b3d868488f");
+#elif UNITY_ANDROID
+            BuglyAgent.InitWithAppId("a8103a785a");
+#endif
 
 		// 如果你确认已在对应的iOS工程或Android工程中初始化SDK，那么在脚本中只需启动C#异常捕获上报功能即可
 		BuglyAgent.EnableExceptionHandler ();
