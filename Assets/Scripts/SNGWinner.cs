3668b9079c2e9f5b553ca6ff3d47f97c64079b90
rfHu <2449663340@qq.com>
2017-07-12 16:09:20 +0800

    截图

--- Assets/Scripts/SNGWinner.cs
+++ Assets/Scripts/SNGWinner.cs
@@ -84,7 +84,7 @@ public class SNGWinner : MonoBehaviour {
 #if UNITY_IOS
 		Commander.Shared.ShareSNGResult();
 #elif UNITY_ANDROID
-        CaptureByUnity("test");
+        CaptureByUnity();
 #else
 				return null;
 #endif
@@ -117,39 +117,13 @@ public class SNGWinner : MonoBehaviour {
         }
     }
 
-    private void CaptureByUnity(string mFileName)
+    private void CaptureByUnity()
     {
         //获取系统时间并命名相片名  
-        System.DateTime now = System.DateTime.Now;
-        string times = now.ToString();
-        times = times.Trim();
-        times = times.Replace("/", "-");
-        string filename = "poker" + times + ".png";
+        int now = System.DateTime.Now.Millisecond;
+        string filename = "poker" + now + ".png";
+        Application.CaptureScreenshot(filename, 0);
 
-        Application.CaptureScreenshot(mFileName, 0);
-        Debug.Log(Application.persistentDataPath);
         Commander.Shared.ShareSNGResult(Application.persistentDataPath + "/" + filename);
     }  
-
-    private void SharePicforAndroid()
-    {
-
-
-        //截取屏幕  
-        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBAHalf, false);
-        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        texture.Apply();
-        //转为字节数组  
-        byte[] bytes = texture.EncodeToPNG();
-
-        //string destination = "/sdcard/texas.poker.top/sharePhoto";
-        ////判断目录是否存在，不存在则会创建目录  
-        //if (!Directory.Exists(destination))
-        //{
-        //    Directory.CreateDirectory(destination);
-        //}
-        //string Path_save = destination + "/" + filename;
-        //存图片  
-        //System.IO.File.WriteAllBytes(Path_save, bytes);
-    }
 }
