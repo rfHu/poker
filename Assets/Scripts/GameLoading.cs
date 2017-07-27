3502045bee1b3048bb1fdc69dad0ad3886f40987
朱思能 <158590301@qq.com>
2017-07-27 11:37:26 +0800

    cin

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -12,9 +12,9 @@ public class GameLoading : MonoBehaviour {
 			external.SetProxy("http://localhost:8888");
 			// external.SetSid("s%3ASuOxhyXR0wmfu1xIZZUmFFdAq8Cd5xsD.h6RldH4l7niXxoZG0QyYnlt36QayIW0rtY0Z8GmorV0");
             // external.SetRoomID("5950a958cfb1ed3a3860c871");
-                        external.SetRoomID("59795083ec3f7a4fbfd4e7a3");
+                        external.SetRoomID("597957abd413501e4ab8fdf1");
 
-            external.SetSid("s%3AnLIz4iurIvlh-akDv1JlpeaaYslzZr4H.LtyNbSwQw8%2BCncnhLhZ5Hr9no0ZrjJCwglJpv96kdhY");
+            external.SetSid("s%3AEX8lAI1wgdFE70m8InJRILHiyWbQOK3A.haiE%2Byf9ZxiHYeQQ2OW55P0YQi6ZbrOn38Tefn8X0qM");
 
             // external.SetSid("s%3A5oV4waP2FdytuZ_daHx1cOgW_0JwkJJ3.x7y5%2B%2BoEaDDtAZetL8CAiJVX2ZMOrUds%2BNP5IzyNjyc");
             // external.SetRoomID("595f73098425856d1c241a49");
