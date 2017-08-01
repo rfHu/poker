4d2258d477b674ad837954cd819d1208e61be080
朱思能 <158590301@qq.com>
2017-08-01 19:19:30 +0800

    表情发不出bug修复

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -16,7 +16,7 @@ public class GameLoading : MonoBehaviour {
 			external.SetSocket("https://socket.dev.poker.top");
 			external.SetProxy("http://localhost:8888");
 
-            var rid = "59804f5900f440257922a8db";
+            var rid = "5980582cc6192326a6f96187";
             var sid = "s%3AEWngTSmC--vE3LgLg-y3HSO3Gq15XKLF.HcRsejXo7DBWg0ZyT70IzRUJ6ORcZQJzT1kJDGTuqCI";
 			// var sid = "s%3AXhe64r-343VHqshGoAhAEmQgTJmdb5Fm.z056Nsm%2F8BplUVSwqAJeL7TXGPtgWOKuim%2B6DoyMMbc";
 
@@ -24,7 +24,7 @@ public class GameLoading : MonoBehaviour {
 			// var sid = "s%3AHlY6SR0V3m8oM2oofbX_yl5R7f6v6Q7R.PK%2FqqIiSZHB0zLgH%2BwV52Yesi3CcsTPJFC3JPb7tjSQ";
 
             //external.InitGame(rid + "&" + sid);
-			 external.InitMatch(rid + "&" + sid);
+			 external.InitGame(rid + "&" + sid);
 		#endif
 
 		// 开启SDK的日志打印，发布版本请务必关闭
