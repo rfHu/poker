057c3e3c363104a1445664039e817e17b23738df
朱思能 <158590301@qq.com>
2017-08-22 15:26:52 +0800

    优化

---
+++ Assets/Scripts/ButtonColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI.ProceduralImage;
+using UniRx;
+
+public class ButtonColor : MonoBehaviour {
+	[SerializeField]private ProceduralImage border;
+	private ProceduralImage bgImage;
+
+	void Start () {
+		bgImage = GetComponent<ProceduralImage>();
+
+		GameSetting.TableSprite.Subscribe((sprite) => {
+			if (sprite == 0) {
+				bgImage.color = _.HexColor("#1495f7");
+				border.color = _.HexColor("#78c4ff");
+			} else if (sprite == 1) {
+				bgImage.color = _.HexColor("#3fa360");
+				border.color = _.HexColor("#5fce83");
+			}
+		}).AddTo(this);
+	}
+}
