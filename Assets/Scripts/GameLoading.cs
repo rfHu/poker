e98b3ea5c186ae5f6ca08d53f2f72987127b330d
rfHu <2449663340@qq.com>
2017-08-30 14:05:31 +0800

    删除无用图片

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -56,7 +56,7 @@ public class GameLoading : MonoBehaviour {
     private void debugSetup() {
         External.Instance.SetSocket("https://socket.dev.poker.top");
         External.Instance.SetProxy("http://localhost:8888");
-        var rid = "59a627ced3603332aae02e93";
+        var rid = "59a637343415b135a224a36b";
         var sid = "s%3ApZ3jjxrF8jFvB7X8YgHQ6HOsvt8EwutY.yghLdSzleA17GpUBWW5KSqufiNoEOruhL%2Byo6ysgjA0";
 
         External.Instance.InitGame(rid + "&" + sid);
