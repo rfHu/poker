a8f364c4dc0582f63c1a77229e88e0b01ccbc6cd
rfHu <2449663340@qq.com>
2017-09-12 12:00:59 +0800

    添加新音乐

--- Assets/Scripts/EmoticonEvent.cs
+++ Assets/Scripts/EmoticonEvent.cs
@@ -60,4 +60,34 @@ public class EmoticonEvent : MonoBehaviour {
     {
         G.PlaySound("mf_touji");
     }
+
+    public void SharkEmo2() 
+    {
+        PoolMan.Spawn("Emo15_Shark");
+    }
+
+    public void GanbeiSound()
+    {
+        G.PlaySound("mf_ganbei");
+    }
+
+    public void DiaoyuSound()
+    {
+        G.PlaySound("mf_diaoyu");
+    }
+
+    public void DianzanSound()
+    {
+        G.PlaySound("mf_dianzan");
+    }
+
+    public void ShitSound()
+    {
+        G.PlaySound("mf_shit");
+    }
+
+    public void SharkSound()
+    {
+        G.PlaySound("mf_shark");
+    }
 }
