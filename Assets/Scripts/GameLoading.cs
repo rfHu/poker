c6b03d199f291f40718bdf4cdd04e07eca40d6ea
朱思能 <158590301@qq.com>
2017-09-20 15:19:30 +0800

    cin

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -85,7 +85,7 @@ public class GameLoading : MonoBehaviour {
         #if UNITY_EDITOR 
 			External.Instance.SetSocket("https://socket.dev.poker.top");
 			External.Instance.SetProxy("http://localhost:8888");
-			var rid = "59c20da6c3d8f066fd66bb63";
+			var rid = "59c1e1928874674cfac303b3";
 			var sid = "s%3AsAgz8nuZ15cD6lyIv6zC1hfUABIpTukA.cXisQhX16Lgqf1YB8ZjZzw4I1%2FIe40%2FHyQDQzRnPpXo";
 
 			External.Instance.InitGame(rid + "&" + sid);
