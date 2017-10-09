3354c667bf763e0ae18ac3ff95cffef326d34856
rfHu <2449663340@qq.com>
2017-10-09 18:15:31 +0800

    MTT页面修改

--- Assets/Scripts/MTTMsgPage/MTTCellView.cs
+++ Assets/Scripts/MTTMsgPage/MTTCellView.cs
@@ -12,18 +12,18 @@ namespace MTTMsgPage
     {
         protected MTTPageData _data;
 
-        protected ProceduralImage _produralImage;
+        protected Image _Image;
 
         public virtual void SetData(MTTPageData data)
         {
             _data = data;
-            _produralImage.enabled = data.needbg;
+            _Image.enabled = data.needbg;
         }
 
         override public void CollectViews() 
         {
             base.CollectViews();
-            _produralImage = root.GetComponent<ProceduralImage>();
+            _Image = root.GetComponent<Image>();
         }
 
         public abstract bool CanPresentModelType(Type modelType);
