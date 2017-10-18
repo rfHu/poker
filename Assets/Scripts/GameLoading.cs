9eb6f4d64503b3f2951ff960d4cba7c4cb4977b6
rfHu <2449663340@qq.com>
2017-10-18 16:56:34 +0800

    牌局回顾兼容

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -90,7 +90,7 @@ public class GameLoading : MonoBehaviour {
 
 		#if UNITY_EDITOR
 			// // 切换房间测试逻辑
-			var rid = "59e6cfb5d19c3d7169155e43";
+			var rid = "59e49da543842a10fff51631";
 			var sid = "s%3AsF7HMvYzII20fOFynbzgvaJd8qAo09kk.IT7xTx2KDUjs2KPHZA6qmuj1FCC5ySCOR%2F8gYs69J7Q";
 			External.Instance.InitGame(rid + "&" + sid);
 
@@ -103,8 +103,8 @@ public class GameLoading : MonoBehaviour {
 			External.Instance.SetSocket("https://socket.dev.poker.top");
 			External.Instance.SetProxy("http://localhost:8888");
 
-            var rid = "59e708368f686d02a78ac09c";
-            var sid = "s%3AsF7HMvYzII20fOFynbzgvaJd8qAo09kk.IT7xTx2KDUjs2KPHZA6qmuj1FCC5ySCOR%2F8gYs69J7Q";
+            var rid = "59e714478f686d02a78ac386";
+            var sid = "s%3A_iit6sMSc2ipvTGw-KcOETDwDVFl9zGr.wzzldmlsK2HpLXo7KUa2iTHC7lWuHbiqMiAC2BOyIHY";
 
             //External.Instance.InitGame(rid + "&" + sid); 
         External.Instance.InitGame(rid + "&" + sid);
