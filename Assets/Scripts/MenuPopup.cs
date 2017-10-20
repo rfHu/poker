07143da015515d28c88c22edd4b30cbf7f3e4515
rfHu <2449663340@qq.com>
2017-10-20 16:46:05 +0800

    cin

--- Assets/Scripts/MenuPopup.cs
+++ Assets/Scripts/MenuPopup.cs
@@ -115,7 +115,7 @@ public class MenuPopup : MonoBehaviour {
 
         fitState(GameData.Shared.SelfState.Value);
 
-        ShareGo.SetActive(GameData.Shared.ClubID != "");
+        ShareGo.SetActive(GameData.Shared.ClubID == "");
     }
 
 	void Awake()
