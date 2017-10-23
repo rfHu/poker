dabda1db0829d958f4c52001ead61b0ca439d837
rfHu <2449663340@qq.com>
2017-10-23 18:56:57 +0800

    兼容限注

--- Assets/Scripts/GameLoading.cs
+++ Assets/Scripts/GameLoading.cs
@@ -103,8 +103,8 @@ public class GameLoading : MonoBehaviour {
 			External.Instance.SetSocket("https://socket.dev.poker.top");
 			External.Instance.SetProxy("http://localhost:8888");
 
-            var rid = "59ed892c2836b64085afb836";
-            var sid = "s%3AfBS158gOaYKcplSt1dNjigd8EW_85TG7.%2B1JQAbSFJb9AL0hUaSpSRakH8aYaGGcsxXUWUryqnRw";
+            var rid = "59edbe0d3174e14ab1230ed8";
+            var sid = "s%3A22aproA498qhMWzgvWWwprCHtzj56Kmz.m%2BZ72%2BWbGrDoII85BJ5%2FCkZqP0QaXO7IHZyda369jKY";
             //var cid = "590310cef0fe643c39829720";
             //External.Instance.InitGame(rid + "&" + sid); 
         External.Instance.InitGame(rid + "&" + sid);
